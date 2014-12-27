97a990259a3f08d1bbd4c084465d4efd8b1ef4f8
jonatan27 <klitsamuel11@gmail.com>
2014-12-27 21:07:18 +0100

    Jeg har lavet projektet

---
+++ Klik spil/Klik spil/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+
+    'Hvor mange klik man har
+    Dim clicks As Integer = 0
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
