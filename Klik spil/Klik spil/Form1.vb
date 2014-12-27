7411d7b22aef2566ebb44b08703d129268a42a73
SamuelKlit <samuel-er@live.dk>
2014-12-27 21:16:48 +0100

    Tilføjet shop

--- Klik spil/Klik spil/Form1.vb
+++ Klik spil/Klik spil/Form1.vb
@@ -1,7 +1,10 @@
 ﻿Public Class Form1
 
     'Hvor mange klik man har
-    Dim clicks As Integer = 0
+    Public clicks As Integer = 0
+
+    'Hvor mange power clicks man har
+    Public powerclicks As Integer = 0
 
     Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
 
---
+++ Klik spil/Klik spil/Shop.vb
@@ -0,0 +1,16 @@
+﻿Public Class Shop
+
+    'Prisen på en power klik
+    Dim powerklikPris As Integer = 100
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If Form1.clicks - powerklikPris >= 0 Then
+            Form1.clicks -= powerklikPris
+            Form1.powerclicks += 1
+        End If
+    End Sub
+
+    Private Sub Shop_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
